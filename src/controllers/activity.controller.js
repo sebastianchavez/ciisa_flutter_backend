79f67b0de09a586ed995d647719b92deab3e35b2
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2020-11-15 04:37:34 -0300

    Publicacion masiva de Actividades Lista

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -1,5 +1,6 @@
 const CONSTANTS = require('../config/constants')
 const activityCtrl = {}
+const User = require('../models/User')
 
 const Activity = require('../models/Activity')
 
@@ -13,17 +14,109 @@ activityCtrl.getActivities = async (req, res) => {
     }
     
 };
+//  AQUI ES DONDE TENEMOS UN PROBLEMA AL MOMENTO DE CREARLOS PERO POR ALGUNA RAZON NO AL MOMENTO DE UPDATE
+
+
+activityCtrl.createActivities = async (req, res) => {
+    let users = []
+    const {  title, description, date, initialHour, finishHour} = req.body;
+    try {
+        //const { title, description, date, initialHour, finishHour, _carreer, _subject, _section, _period} = req.body
+        //const { title, description, date, initialHour, finishHour} = req.body;
+        //const newActivity = new Activity(req.body);
+        
+        //const users = await User.find({ segments })
+        /*
+        const newActivity = new Activity({
+            userId,
+            title,
+            description,
+            date,
+            initialHour,
+            finishHour
+        })*/
+        /*
+        var query = {carreer: _carreer, subject: _subject, section: _section, period: _period};
+
+        users = dbo.collection("User").find(query).toArray(
+            // Aqui estoy perdido
+            function(err, result) {
+                if (err) throw err;
+                console.log(result);
+                db.close();
+                //
+
+
+
+            }
+        );
+        
+        //const newActivity = new Activity(req.body)
+        for(var user in users) {
+
+            const newActivity = new Activity({
+                userId: user._id,
+                title,
+                description,
+                date,
+                initialHour,
+                finishHour
+            })
+
+
+            console.log(user);
+            await newActivity.save();
+        }
+        */
+
+
+        //Solo falta filtrar por segmentacion para estar perfecto
+        //me falta poder tener usuarios con segmentacion para arreglarlo
+       //const users = await User.find({ segments })
+       const users = await User.find()
+       
+       for(user in users) {
+
+            user_info = users[user]
+            user_id = user_info._id
+            
+            const newActivity = new Activity({
+                userId: user_id,
+                title,
+                description,
+                date,
+                initialHour,
+                finishHour
+            })
+
+        
+            await newActivity.save();
+        }
+
+
+
+        //await newActivity.save();
+        res.send({message: "Activity Created in Users"});
+        //res.json({ users });
+    } catch (e) {
+        res.status(500).send({ message: CONSTANTS.MESSAGES.ERROR.DEFAULT_MESSAGE })
+    }
+};
+
+/*
 activityCtrl.createActivities = async (req, res) => {
     
     try {
         const newActivity = new Activity(req.body);
         await newActivity.save();
-    
-        res.send({message: "Activity Created"});
+        
+        res.json({ newActivity });
     } catch (e) {
         res.status(500).send({ message: CONSTANTS.MESSAGES.ERROR.DEFAULT_MESSAGE })
     }
 };
+*/
+
 activityCtrl.getActivity = async (req, res) => {
 
     try {
@@ -38,6 +131,7 @@ activityCtrl.getActivity = async (req, res) => {
 activityCtrl.editActivity = async (req, res) => {
 
     try {
+        //await Activity.findByIdAndUpdate(req.params.title, req.body);
         await Activity.findByIdAndUpdate(req.params.id, req.body);
         res.json({status: 'Activity Updated'})
     } catch (e) {
